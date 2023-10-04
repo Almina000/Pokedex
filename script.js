73e6215d88219cbcd312d2503395c457cd65873e
Almina000 <63445752+Almina000@users.noreply.github.com>
2023-10-04 17:57:23 +0200

    Add files via upload
    Backup + first frontend try

---
+++ script.js
@@ -0,0 +1,41 @@
+let score = localStorage.getItem("score") ? parseInt(localStorage.getItem("score")) : 0;
+
+function updateScore() {
+    document.getElementById("score-value").textContent = score;
+}
+
+function saveScore() {
+    localStorage.setItem("score", score);
+}
+
+async function checkPokemon() {
+    const name = document.getElementById("name").value.toLowerCase();
+    const number = document.getElementById("number").value.toLowerCase();
+
+    try {
+        const response = await fetch('pokedex.json');
+        const data = await response.json();
+
+        const pokemon = data.find(entry => entry.name.toLowerCase() === name && entry.number.toLowerCase() === number);
+
+        if (pokemon) {
+            score += pokemon.points;
+            updateScore();
+            saveScore();
+            document.getElementById("result").textContent = `Correct! ${name} is in the Pokédex. You earned ${pokemon.points} points.`;
+        } else {
+            document.getElementById("result").textContent = `Incorrect! ${name} with number ${number} is not in the Pokédex.`;
+        }
+
+    } catch (error) {
+        console.error("Error:", error);
+    }
+
+    document.getElementById("name").value = "";
+    document.getElementById("number").value= "";
+}
+
+// Initialisiere den Punktestand beim Laden der Seite
+window.onload = function() {
+    updateScore();
+};
